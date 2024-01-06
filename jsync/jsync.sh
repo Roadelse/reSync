ca59cd70a4d12d436521afb4dc5656c130f6efa9
Roadelse <roadelse@HaSee-GX8.com>
2024-01-06 23:32:16 +0800

    inDev, finish to rebuild jsync

--- jsync/jsync.sh
+++ jsync/jsync.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-# This script aims to simplify rsync command based on project-level pre-defined paths among multiple machines
+# This script aims to simplify rsync command based on project-level pre-defined paths among multiple hosts
 
 # -m : mode, get/put, put2 (automatically create all missing directories)
 # -t : target in .jsyncrc
 
 # 2022-12-21    init
-# 2024-01-05    optimized & add support for updating default target in .jsyncrc 
+# 2024-01-06    rebuild. Support multiple targets, auto-move last-used target to top, local transfer, relative transfer. Skeleton comments are reformatted
 
 
 
-# <L1> static parameters
+###########################################################
+# Prepare
+###########################################################
+# >>>>>>>>>>>>> pre-settings & static params <<<<<<<<<<<<<<
+set -e
+
 configFile=.jsyncrc
+exe=$0
 
 
-# <L1> search configFile from PWD to /
+# >>>>>>>>>>>> search configFile from PWD to / <<<<<<<<<<<<
 tarDir=${PWD}
 rPath=""
 while [[ $tarDir != '/' ]]
@@ -26,25 +32,27 @@ do
     tarDir=`dirname $tarDir`
 done
 
-if [[ ! -e $tarDir/$configFile ]]; then
-    echo "cannot find $configFile in the current and all parent directories! Error"
-    exit 101
-fi
-
-echo "configFile : $tarDir/$configFile"
-
-# <L1> show configFile content if no argument specified
+# >>>>>>>>>>>>>>>> show configFile content <<<<<<<<<<<<<<<<
+# if no argument specified and $configFile existed
 if [[ -z "$@" ]]; then
-    cat $tarDir/$configFile
+    if [[  -e $tarDir/$configFile ]]; then
+        echo "configFile : $tarDir/$configFile"
+        cat $tarDir/$configFile
+    else
+        echo "Not within a jsync project path, exit"
+    fi
     exit 0
 fi
 
 
-# <L1> resolve CLI arguments using getopts
+# >>>>>>>>>>>> resolve CLI arguments and check <<<<<<<<<<<<
+# ================== default arg-relative variables
 mode=put
 target=default
 unittest=0
-while getopts "m:t:u" arg
+echo_only=0
+# ================== getopts resolution
+while getopts "m:t:ue" arg
 do
     case $arg in 
         m)
@@ -53,90 +61,195 @@ do
             target=$OPTARG;;
         u)
             unittest=1;;
+        e)
+            echo_only=1;;
     esac
 done
-shift ((OPTIND-1))
+# ================== post-processing
+if [[ $unittest == 1 && $OPTIND -gt 2 ]]; then
+    echo "Error! -u can only be used alone"
+
+    exit 200
+fi
+# ~~~~~~~~~~ adjust arg index
+shift $((OPTIND-1))
 
 
-if [[ $unittest == "0" ]]; then
 
-# <L1> Confirm mode & target
+###########################################################
+# Core logic: 
+###########################################################
+if [[ $unittest == "0" ]]; then #>- not in unittest mode
+# >>>>>>>>>>>>>>>>> Confirm mode & target <<<<<<<<<<<<<<<<<
 addr=
 while read line
 do
     eList=($line)
-    # <L2> if no target specified
+    # ================== use 1st target if not specified
     if [[ $target == default ]]; then
-        echo ${line}
+        echo "target detected: ${line}"
         addr=${eList[1]}
         break
     fi
-    # <L2> find target and move it top top
+
+    # ================== find target and move it to top
     if [[ "${eList[0]}" == "$target" ]]; then
         addr=${eList[1]}
-        awk 'NR==1{print s} $0!~s' s="$line" > $tarDir/$configFile
+        awk 'NR==1{print s} $0!~s' s="$line" $tarDir/$configFile > $tarDir/$configFile.2  #>- move target line to top
+        mv -f $tarDir/$configFile.2 $tarDir/$configFile
         break
     fi
 done < $tarDir/$configFile
 
-# <L2> cannot find target
+# ================== cannot find target
 if [ -z "$addr" ]; then
     echo "cannot find target <$target>"
     exit 102
 fi
 
-# <L1> call rsync based on mode & target
-# <L2> handle target files 
+
+# >>>>>>>>>>> call rsync based on mode & target <<<<<<<<<<<
+# ================== handle target files 
 targetFiles="$@"
 if [[ $targetFiles == "" ]]; then
     targetFiles="*"
 fi
 
-# <L2> put mode
+targetPaths=()
+for a in "$targetFiles"; do
+    # echo "realpath \"$a\" --relative-to \"$tarDir\""
+    targetPaths+=(`realpath -m "$a" --relative-to "$tarDir"`)
+done
+
+# ================== put mode
 if [[ "$mode" =~ put.* ]]; then
     echo "entering <put> mode"
-    rsync -avPL $targetFiles $addr/$rPath
+    
+    echo "exec: rsync -avPLR \"${targetPaths[@]}\" $addr"
+    # ~~~~~~~~~~ do not call rsync if echo_only == 1
+    if [[ $echo_only == 0 ]]; then
+        cd $tarDir #>- Must do the rsync in project directory for relative rsync
+        rsync -avPLR "${targetPaths[@]}" $addr
+    fi
 
-# <L2> put2 mode
-elif [[ "$mode" == "put2" ]]; then
-    echo "entering <put2> mode"
-    mach_addr=(${addr//:/ })
-    mach=${mach_addr[0]}
-    ssh $mach "mkdir -p ${mach_addr[1]}/$rPath"
-    rsync -avPL $targetFiles $addr/$rPath
-
-# <L2> get mode
+# ================== get mode
 elif [[ "$mode" == 'get' ]]; then
-    for npat in "$@"; do  #>- npat: name pattern
-        echo "rsync -avPL $addr/$rPath/$targetFiles ."
-        # echo $addr
-        # echo $rPath
-        # echo $targetFiles
-        rsync -avPL $addr/$rPath/$npat .
+    echo "entering <get> mode"
+
+    # ~~~~~~~~~~ complete the remote address
+    # the "/./" is NECESSARY for relative rsync!
+    for i in ${!targetPaths[@]}; do
+        targetPaths[$i]=${addr}/./${targetPaths[$i]}
     done
+    
+    echo "exec: rsync -avPLR \"$targetPaths\" $tarDir"
+    # ~~~~~~~~~~ do not call rsync if echo_only == 1
+    if [[ $echo_only == 0 ]]; then
+        rsync -avPLR "$targetPaths" $tarDir
+    fi
 else
     echo "Error! Unknown mode: $mode"
     exit 103
 fi
 
 
+
+# >>>>>>>>>>>>>>>>>>>>>>> unittest <<<<<<<<<<<<<<<<<<<<<<<<
 else
-    # <L1> unittest
+    # ================== local test
+    # ~~~~~~~~~~ prepare files & dirs
     mkdir jsync_utest && cd $_
     mkdir -p dir1/a dir1/b dir2/c dir2/d
     touch dir1/x dir1/a/y dir2/z dir2/d/m
 
     cd dir1
+
+    # ~~~~~~~~~~ write .jsyncrc
     cat > .jsyncrc << EOF
 plh1 ababa:/home
-test ${PWD}/dir2
+test1 ${PWD/%dir1/dir2}
 EOF
-    jsync -m get -t test d
-    if [[ -e d/e ]]; then
-        echo "mode:get test passed"
+    # ~~~~~~~~~~ call jsync & check results
+    $exe -m get -t test1 d/m
+    if [[ -e d/m ]]; then
+        echo -e "\033[32m mode:get test passed \033[0m"
     else
-        echo "mode:get test failed"
+        echo -e "\033[31m mode:get test failed \033[0m"
         exit 200
     fi
-    if [[ `head -n 1 .jsyncrc` =~ test.* ]]
+
+    # ~~~~~~~~~~ call jsync & check results
+    if [[ `head -n 1 .jsyncrc` =~ test1.* ]]; then
+        echo -e "\033[32m move target test passed \033[0m"
+    else
+        echo -e "\033[31m move target test failed \033[0m"
+        exit 200
+    fi
+
+    # ~~~~~~~~~~ call jsync & check results
+    $exe a/y x
+    if [[ -e ../dir2/x && -e ../dir2/a/y ]]; then
+        echo -e "\033[32m mode:put test passed \033[0m"
+    else
+        echo -e "\033[31m mode:put test failed \033[0m"
+        eixt 200
+    fi
+   
+    # ~~~~~~~~~~ call jsync & check results
+    rm -rf ../dir2/*
+    $exe *
+    if [[ -e ../dir2/x && -e ../dir2/a/y && -e ../dir2/d/m ]]; then
+        echo -e "\033[32m mode:put with expaned wildcard test passed \033[0m"
+    else
+        echo -e "\033[31m mode:put with expaned wildcard test failed \033[0m"
+        eixt 200
+    fi
+
+    # ~~~~~~~~~~ call jsync & check results
+    rm -rf ../dir2/*
+    $exe \*
+    if [[ -e ../dir2/x && -e ../dir2/a/y && -e ../dir2/d/m ]]; then
+        echo -e "\033[32m mode:put with non-expaned wildcard test passed \033[0m"
+    else
+        echo -e "\033[31m mode:put with non-expaned wildcard test failed \033[0m"
+        eixt 200
+    fi
+
+    # ~~~~~~~~~~ local test summary
+    echo -e "\033[32m All local tests passed! \033[0m"
+
+    # ================== remote test if cluster specified (Must be set already in ~/ssh/.config)
+    if [[ -n $1 ]]; then  #>- a remote cluster provided
+        # ~~~~~~~~~~ check connection availability
+        if [[ `ssh $1 "echo 123 && mkdir -p ~/.jsync_utest/r1/r2/L" 2>/dev/null` == "123" ]]; then
+            echo "start to test via ssh based on given cluster"
+        else
+            echo "\033[31m Cannot ssh into provided host: $1, please check your ~/.ssh/config and make sure it can be login without any confirmation. \033[0m"
+            exit 200
+        fi
+
+        # ~~~~~~~~~~ append remote project into .jsyncrc
+        cat >> .jsyncrc << EOF
+test2 $1:~/.jsync_utest
+EOF
+        # ~~~~~~~~~~ test logic
+        mkdir r1 && cd $_
+        # ... get remote files
+        $exe -m get -t test2 r2/L
+        # ... check local files
+        if [[ -e r2/L ]]; then
+            echo -e "\033[32m mode:get for remote test passed \033[0m"
+        else
+            echo -e "\033[31m mode:get for remote test failed \033[0m"
+            exit 200
+        fi
+        # ~~~~~~~~~~ remove remote side-effect
+        ssh $1 "rm -rf ~/.jsync_utest" 2>/dev/null
+        # ~~~~~~~~~~ remote test Summary
+        echo -e "\033[32m All remote tests passed! \033[0m"
+    fi
+
+    # ================== final hint
+    echo -e "\033[33m run 'rm -rf .jsync_utest' to delete the utest directory \033[0m"
 fi
+
