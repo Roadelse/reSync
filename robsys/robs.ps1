8a248295e9f7dea132d39929bc8f55fdf221eb94
Roadelse <roadelse@HaSee-GX8.com>
2024-01-07 23:19:53 +0800

    inDev, improving robsys

--- robsys/robs.ps1
+++ robsys/robs.ps1
@@ -14,31 +14,29 @@
 ###########################################################
 # Preparation
 ###########################################################
+# >>>>>>>>>>>>>>>>>>>> params resolve <<<<<<<<<<<<<<<<<<<<<
+param (
+    # [ValidateSet("sort", "show", "restore", "pack")]
+    [string]$action,
+    [string]$target,
+    [Alias("h")]
+    [switch]$help,
+    [Alias("eo")]
+    [switch]$echo_only,
+    [switch]$error_stop,
+    [Alias("v")]
+    [switch]$verbose,
+    [string]$restore_op
+)
+
 
 # >>>>>>>>>>>>>>>>>>>>> pre-settings <<<<<<<<<<<<<<<<<<<<<<
 Import-Module rdee
 
 
-# >>>>>>>>>>>>>>>>>>>> params resolve <<<<<<<<<<<<<<<<<<<<<
-param (
-    [Alias("e")]
-    [switch]$echo_only,
-    [Alias("t")]
-    [string]$target,
-    [Alias("r")]
-    [switch]$restore_mode,
-    [Alias("rm")]
-    [switch]$restore_move,
-    [switch]$error_stop,
-    [switch]$verbose,
-    [Alias("sr")]
-    [switch]$staticRecall
-)
-
-
 # >>>>>>>>>>>>>>>>>>> global variables <<<<<<<<<<<<<<<<<<<<
 # ================== r.o.b.s. root paths
-$rrL = "D:\recRoot\Roadelse"
+$rrR = "D:\recRoot\Roadelse"
 $rrO = "C:\Users\${env:UserName}\OneDrive\recRoot\Roadelse"
 $rrB = "D:\BaiduSyncdisk\recRoot\Roadelse"
 $rrS = "D:\recRoot\StaticRecall"
@@ -50,55 +48,120 @@ $ignore_global = @(".reconf$", ".resync_config$", "resort-error\..*\.log$", "^pr
 
 
 # >>>>>>>>>>>>>>>>>>>> pre-processing <<<<<<<<<<<<<<<<<<<<<
-# ================== set $pwd as default target if not specified
-if ($target -eq "") {
-    $target = "."
-}
-
-# ================== check target existence
-if (-not (Test-Path -Path $target)) {
-    Write-Output "target path doesn't exist!"
-    exit 1
-}
-
-# ================== get target directory path
-$target_dir = (Get-Item $target).FullName
-if ((Get-Item $target_dir).PSIsContainer) { #>- pass for directory
-    # pass
-} elseif ((Get-Item $target).Name -eq ".reconf") {  #>- get directory path for .reconf
-    $target_dir = (Get-Item $target_dir).DirectoryName
-} else { #>- error for file not being .reconf
-    Write-Error "If a file is given, is must be a .reconf file!" -ErrorAction Stop
-}
-
-# ================== convert all obs path to roadelse path, i.e., local path
-if ($target.FullName.StartsWith($rrO)) {
-    $target_dir = $target_dir.Replace($rrO, $rrL)
-} elseif ($target.FullName.StartsWith($rrB)) {
-    $target_dir = $target_dir.Replace($rrB, $rrL)
-} elseif ($target.FullName.StartsWith($rrS)) {
-    $target_dir = $target_dir.Replace($rrS, $rrL)
-} elseif ($target.FullName.StartsWith($rrL)) {
-} else {  #>- Error if not within r.o.b.s. system
-    Write-Error "Path not in r.o.b.s. system! $target_dir" -ErrorAction Stop
-}
-
-
 
 
 ###########################################################
 # kernel functions
 ###########################################################
 # >>>>>>>>>>>>>>>>>>>>>>>>> entry <<<<<<<<<<<<<<<<<<<<<<<<<
-function main {
+# function main {
     
-    if ($restore_mode) {
-        restore_dir $target_dir ($restore_move ? "Move" : "Copy")
-    } elseif ($staticRecall) {
-        pack2StaticRecall $target_dir
+#     if ($mode.ToLower() -eq "sort"){
+#         sort_dir @$PSBoundParameters
+#     }
+
+#     if ($restore_mode) {
+#         restore_dir $target_dir ($restore_move ? "Move" : "Copy")
+#     } elseif ($staticRecall) {
+#         pack2StaticRecall $target_dir
+#     } else {
+#         sort_dir $target_dir
+#     }
+# }
+# <L1> global help function
+function show_help {
+    param(
+        [string]$action
+    )
+    if ($action -eq "") {
+        Write-Output @"
+[~] Usage
+    robs.ps1 <action> [<target>] [options]
+
+Supported actions:
+    ● sort      robs.ps1 sort -h|--help
+    ● restore   robs.ps1 restore -h|--help
+    ● pack      robs.ps1 pack -h|--help
+    ● show      robs.ps1 pack -h|--help
+
+Global options:
+    ● -eo, -echo_only
+        Do not do the execution, but only echo the commands
+    ● -h, -help
+        Show the help information for this script
+"@
+    } elseif ($action -eq "sort") {
+        Write-Output @"
+        [~] Usage
+            robs.ps1 sort [<target>] [options]
+"@
     } else {
-        sort_dir $target_dir
+        Write-Error "Should Never be displayed!" -ErrorAction Stop
     }
+}
+
+
+function norm_path{
+    param(
+        [string]$target,
+        [ValidateSet("skip", "mkdir", "stop")]
+        [string]$no_exist_action="mkdir"
+    )
+
+# ================== set $pwd as default target if not specified
+if ($target -eq "") {
+    $target = "."
+}
+
+
+# ================== get target directory path
+$target = [IO.Path]::GetFullPath($target, $pwd.ProviderPath)
+
+if (Test-Path $target){
+    if ((Get-Item $target).PSIsContainer) { #>- pass for directory
+        # pass
+    } elseif ((Get-Item $target).Name -eq ".reconf") {  #>- get directory path for .reconf
+        $target = (Get-Item $target).DirectoryName
+    } else { #>- error for file not being .reconf
+        Write-Error "If a file is given, is must be a .reconf file!" -ErrorAction Stop
+    }
+}
+
+# ================== convert all obs path to roadelse path, i.e., local path
+if ($target.StartsWith($rrO)) {
+    $rpath = $target.Replace($rrO, $rrR)
+    $opath = $target
+    $bpath = $target.Replace($rrO, $rrB)
+    $spath = $target.Replace($rrO, $rrS)
+} elseif ($target.StartsWith($rrB)) {
+    $rpath = $target.Replace($rrB, $rrR)
+    $opath = $target.Replace($rrB, $rrO)
+    $bpath = $target
+    $spath = $target.Replace($rrB, $rrS)
+} elseif ($target.StartsWith($rrS)) {
+    $rpath = $target.Replace($rrS, $rrR)
+    $opath = $target.Replace($rrS, $rrO)
+    $bpath = $target.Replace($rrS, $rrB)
+    $spath = $target
+} elseif ($target.StartsWith($rrR)) {
+    $rpath = $target
+    $opath = $target.Replace($rrR, $rrO)
+    $bpath = $target.Replace($rrR, $rrB)
+    $spath = $target.Replace($rrR, $rrS)
+} else {  #>- Error if not within r.o.b.s. system
+    Write-Error "Path not in r.o.b.s. system! $target" -ErrorAction Stop
+}
+
+# ================== check target existence
+if (-not (Test-Path -Path $rpath)) {
+    if ($no_exist_action -eq "mkdir"){
+        New-Item -ItemType Directory $rpath -Force
+    } elseif ($no_exist_action -eq "stop"){
+        Write-Error "target path doesn't exist!" -ErrorAction Stop
+    } 
+}
+
+    return $rpath, $opath, $bpath, $opath
 }
 
 
@@ -113,28 +176,38 @@ function sort_dir {
 
     # ================== parameters definition
     param(
-        [Parameter(Mandatory = $true)]
-        $wdir,
-        $rcf = @{}
+        $target,
+        $rcf = @{},
+        [Alias("h")]
+        [switch]$help,
+        [Parameter(ValueFromRemainingArguments = $true)]
+        $_arg_holders
     )
 
     # Write-Output "Enter sort_dir $wdir"
 
-    # ================== pre-processing
-    # ~~~~~~~~~~ handle r.o.b. paths 
-    if ($wdir -is [string]) {
-        if (-not(Test-Path $wdir)) { #>- for conditions that building local links in a new PC
-            New-Item -ItemType Directory $wdir -Force
-        }
-        $wdir = Get-Item $wdir;
+    if ($help){
+        show_help -action sort
+        return
     }
 
-    $odir_path = l2o $wdir.FullName
-    $bdir_path = l2b $wdir.FullName
+    # ================== pre-processing
+    # ~~~~~~~~~~ handle r.o.b. paths 
+    $rpath, $opath, $bpath, $spath = norm_path $target
+    # $wdir = Get-Item $robs_path[0]
+    # if ($wdir -is [string]) {
+        # if (-not(Test-Path $wdir)) { #>- for conditions that building local links in a new PC
+            # New-Item -ItemType Directory $wdir -Force
+        # }
+        # $wdir = Get-Item $wdir;
+    # }
+
+    # $opath = l2o $wdir.FullName
+    # $bpath = l2b $wdir.FullName
 
     # ~~~~~~~~~~ load .reconf if existed
-    if (Test-Path "${wdir}\.reconf") {
-        Update-Hashtable $rcf (Get-Content "${wdir}\.reconf" | ConvertFrom-Json -AsHashtable)
+    if (Test-Path "${rpath}\.reconf") {
+        Update-Hashtable $rcf (Get-Content "${rpath}\.reconf" | ConvertFrom-Json -AsHashtable)
     }
     
     # ~~~~~~~~~~ check ignore_this and 
@@ -143,10 +216,10 @@ function sort_dir {
     }
 
     # ~~~~~~~~~~ remark current target in global
-    $sorted_dirs.Add($wdir.FullName.Replace($rrL + "\", "")) | Out-Null
+    $sorted_dirs.Add($rpath.FullName.Replace($rrR + "\", "")) | Out-Null
 
     # ================== handle children items one by one
-    Get-ChildItem $wdir | 
+    Get-ChildItem $rpath | 
     ForEach-Object -Process {
         # ~~~~~~~~~~ ignore symlink
         if ($_.Attributes -match "ReparsePoint") {
@@ -168,41 +241,41 @@ function sort_dir {
 
         # ~~~~~~~~~~ Do the operation via manually set rule
         if ($rcf.Contains('OneDrive') -and $rcf.OneDrive.Contains($_.Name)) {
-            move_and_link $_ $_.FullName.Replace($rrL, $rrO)
+            move_and_link $_ $_.FullName.Replace($rrR, $rrO)
             return
         } elseif ($rcf.Contains('Baidusync') -and $rcf.Baidusync.Contains($_.Name)) {
-            move_and_link $_ $_.FullName.Replace($rrL, $rrB)
+            move_and_link $_ $_.FullName.Replace($rrR, $rrB)
             return
         }
 
         # ~~~~~~~~~~ Do the operation via name prefix rule
         if ($_.Name.StartsWith("O..")) {
-            move_and_link $_ $_.FullName.Replace($rrL, $rrO)
+            move_and_link $_ $_.FullName.Replace($rrR, $rrO)
             return
         } elseif ($_.Name.StartsWith("B..")) {
-            move_and_link $_ $_.FullName.Replace($rrL, $rrB)
+            move_and_link $_ $_.FullName.Replace($rrR, $rrB)
             return
         }
 
         # ~~~~~~~~~~ handle children dir recursively
         if ($_.PSIsContainer) {
-            sort_dir -wdir $_ -rcf (deepcopy $rcf)
+            sort_dir -target $_ -rcf (deepcopy $rcf)
 
         # ~~~~~~~~~~ Do the operation in default rules
         } else {
             $fsize = $_.Length / 1MB; # file size in MB
             if ($fsize -lt 5) {
-                move_and_link $_ $_.FullName.Replace($rrL, $rrO)
+                move_and_link $_ $_.FullName.Replace($rrR, $rrO)
             } elseif ($fsize -lt 200) {
-                move_and_link $_ $_.FullName.Replace($rrL, $rrB)
+                move_and_link $_ $_.FullName.Replace($rrR, $rrB)
             }
         }
         # exit 0
     }
 
     # ================== link back items in OneDrive
-    if (Test-Path $odir_path) {
-        Get-ChildItem $odir_path | ForEach-Object -Process {
+    if (Test-Path $opath) {
+        Get-ChildItem $opath | ForEach-Object -Process {
             $_lp = o2l $_.FullName  #>- corresponding local path for $_.FullName
             if (Test-Path $_lp) {
                 # ~~~~~~~~~~ handle shortcut links separately (should only in onedrive!)
@@ -215,17 +288,17 @@ function sort_dir {
                 $i_lp = Get-Item $_lp
                 if (-not ($i_lp.Attributes -match "ReparsePoint")) {
                     if ($i_lp.PSIsContainer) {
-                        if (-not $sorted_dirs.Contains($_lp.Replace($rrL + "\", ""))) {
-                            errHandler ("File conflict: " + $_lp.Replace($rrL + "\", ""))
+                        if (-not $sorted_dirs.Contains($_lp.Replace($rrR + "\", ""))) {
+                            errHandler ("File conflict: " + $_lp.Replace($rrR + "\", ""))
                         }
                     } else {
-                        errHandler ("File conflict: " + $_lp.Replace($rrL + "\", ""))
+                        errHandler ("File conflict: " + $_lp.Replace($rrR + "\", ""))
                     }
                 }
             } else {
                 # ~~~~~~~~~~ do the link operation
                 if ($verbose) {
-                    Write-Output ("link OneDrive item: " + $_lp.Replace($rrL + "\", "") + " to Local")
+                    Write-Output ("link OneDrive item: " + $_lp.Replace($rrR + "\", "") + " to Local")
                 }
                 New-Item -ItemType SymbolicLink -Path $_lp -Target $_.FullName
             }
@@ -233,8 +306,8 @@ function sort_dir {
     }
 
     # ================== link back items in BaiduSync
-    if (Test-Path $bdir_path) {
-        Get-ChildItem $bdir_path | ForEach-Object -Process {
+    if (Test-Path $bpath) {
+        Get-ChildItem $bpath | ForEach-Object -Process {
             Assert (-not $_.FullName.EndsWith(".lnk")) "Shortcut links should not occur in BaiduSync!" #>- ensure no shortcut link
             $_lp = b2l $_.FullName  #>- corresponding local path for $_.FullName
             if (Test-Path $_lp) {
@@ -242,17 +315,17 @@ function sort_dir {
                 $i_lp = Get-Item $_lp
                 if (-not ($i_lp.Attributes -match "ReparsePoint")) {
                     if ($i_lp.PSIsContainer) {
-                        if (-not $sorted_dirs.Contains($_lp.Replace($rrL + "\", ""))) {
-                            errHandler ("File conflict: " + $_lp.Replace($rrL + "\", ""))
+                        if (-not $sorted_dirs.Contains($_lp.Replace($rrR + "\", ""))) {
+                            errHandler ("File conflict: " + $_lp.Replace($rrR + "\", ""))
                         }
                     } else {
-                        errHandler ("File conflict: " + $_lp.Replace($rrL + "\", ""))
+                        errHandler ("File conflict: " + $_lp.Replace($rrR + "\", ""))
                     }
                 }
             } else {
                 # ~~~~~~~~~~ do the link operation
                 if ($verbose) {
-                    Write-Output ("link BaiduSync item: " + $_lp.Replace($rrL + "\", "") + " to Local")
+                    Write-Output ("link BaiduSync item: " + $_lp.Replace($rrR + "\", "") + " to Local")
                 }
                 New-Item -ItemType SymbolicLink -Path $_lp -Target $_.FullName
             }
@@ -294,19 +367,17 @@ function pack2StaticRecall {
     # ================== param resolve
     param(
         [Parameter(Mandatory = $true)]
-        [string]$ldir
+        [string]$target
     )
 
     # ================== pre-processing
-    $odir = l2o $ldir
-    $bdir = l2b $ldir
+    $rpath, $opath, $bpath, $spath = norm_path $target -no_exist_action stop
 
-    Assert (Test-Path $ldir) "Path doesn't exist! $ldir"
-    Assert ((Get-Item $ldir).PSIsContainer) "Path is not a directory! $ldir"
+    Assert ((Get-Item $rpath).PSIsContainer) "Path is not a directory! $rpath"
 
-    $sr_path = ($ldir + ".zip").Replace("recRoot\Roadelse", "recRoot\StaticRecall")
+    $sr_path = ($rpath + ".zip").Replace($rrR, $rrS)
 
-    restore_dir $ldir -mc "Move"
+    restore_dir $rpath -mc "Move"
 
     if ($echo_only) {
         Write-Output "compress & delete, move .zip to StaticRecall"
@@ -314,10 +385,10 @@ function pack2StaticRecall {
     }
 
     # ================== do the operation
-    Compress-Archive -Path $ldir -DestinationPath ($ldir + ".zip") -CompressionLevel Fastest
-    Remove-Item $ldir -Recurse -Force
+    Compress-Archive -Path $rpath -DestinationPath ($rpath + ".zip") -CompressionLevel Fastest
+    Remove-Item $rpath -Recurse -Force
     New-Item -ItemType Directory -Path (Split-Path $sr_path) -Force
-    Move-Item -Path ($ldir + ".zip") -Destination $sr_path
+    Move-Item -Path ($rpath + ".zip") -Destination $sr_path
 }
 
 
@@ -326,21 +397,21 @@ function restore_dir {
     # ================== param resolve
     [CmdletBinding()]
     param (
-        [string]$wdir,
+        [string]$target,
         [ValidateSet("Move", "Copy")] #>- case-insensitive by default , "move", "copy", "MOVE", "copy")]
-        [string]$mc = "Copy"
+        [string]$restore_op = "Copy"
     )
 
     # ================== pre-processing
+    $rpath, $opath, $bpath, $spath = norm_path $target
+
     if ($echo_only) {
-        Write-Output "restore_dir($wdir, $mc)"
+        Write-Output "restore_dir($rpath, $restore_op)"
         return
     }
 
-    $wdir = $wdir -is [string] ? (Get-Item $wdir) : $wdir
-
     # ================== handle <r.> items one by one
-    Get-ChildItem $wdir | 
+    Get-ChildItem $rpath | 
     ForEach-Object -Process {
         if ($_.Attributes -match "ReparsePoint") {
             $tgt = $_.Target
@@ -348,37 +419,37 @@ function restore_dir {
                 return
             }
             Remove-Item $_
-            if ($mc.ToUpper() -eq "MOVE") {
+            if ($restore_op.ToUpper() -eq "MOVE") {
                 Move-Item -Path $tgt -Destination $_.FullName
             } else {
                 Copy-Item -Path $tgt -Destination $_.FullName -Recurse
             }
         } elseif ($_.PSIsContainer) { #>- for directory
-            restore_dir $_ $mc
+            restore_dir $_ $restore_op
         } elseif ($_.Name.EndsWith(".lnk")) { #>- For Shortcut links
-            if ($mc.ToUpper() -eq "MOVE") {
-                Remove-Item $_.FullName.Replace($rrL, $rrO)
+            if ($restore_op.ToUpper() -eq "MOVE") {
+                Remove-Item $_.FullName.Replace($rrR, $rrO)
             }
         }
     }
 
     # ================== handle <o.> items one by one
-    if (Test-Path (l2o $wdir)) {
-        $nFilesO = (Get-ChildItem -Path (l2o $wdir) -Recurse -File | Measure-Object).Count
+    if (Test-Path $opath) {
+        $nFilesO = (Get-ChildItem -Path $opath -Recurse -File | Measure-Object).Count
         if ($nFilesO -gt 0) {
-            Write-Host ("$nFilesO remained in OneDrive: " + (l2o $wdir)) -ForegroundColor ($restore_move ? "Red" : "Yellow")
+            Write-Host "$nFilesO remained in OneDrive: $opath" -ForegroundColor ($restore_move ? "Red" : "Yellow")
         } else {
-            Remove-Item (l2o $wdir) -Recurse -Force
+            Remove-Item $opath -Recurse -Force
         }
     }
 
     # ================== handle <b.> items one by one
-    if (test-path (l2b $wdir)) {
-        $nFilesB = (Get-ChildItem -Path (l2b $wdir) -Recurse -File | Measure-Object).Count
+    if (test-path $bpath) {
+        $nFilesB = (Get-ChildItem -Path $bpath -Recurse -File | Measure-Object).Count
         if ($nFilesB -gt 0) {
-            Write-Host ("$nFilesO remained in Baidusync: " + (l2b $wdir)) -ForegroundColor ($restore_move ? "Red" : "Yellow")
+            Write-Host "$nFilesO remained in Baidusync: $bpath" -ForegroundColor ($restore_move ? "Red" : "Yellow")
         } else {
-            Remove-Item (l2b $wdir) -Recurse -Force
+            Remove-Item $bpath -Recurse -Force
         }
     }
 }
@@ -400,7 +471,7 @@ function l2o {
         [Parameter(Mandatory = $true)]
         [string]$ldir
     )
-    return $ldir.Replace($rrL, $rrO)
+    return $ldir.Replace($rrR, $rrO)
 }
 
 function o2l {
@@ -409,7 +480,7 @@ function o2l {
         [string]$ldir
     ) 
 
-    return $ldir.Replace($rrO, $rrL)
+    return $ldir.Replace($rrO, $rrR)
 }
 
 function l2b {
@@ -418,7 +489,7 @@ function l2b {
         [string]$ldir
     )
 
-    return $ldir.Replace($rrL, $rrB)
+    return $ldir.Replace($rrR, $rrB)
 }
 
 function b2l {
@@ -427,13 +498,23 @@ function b2l {
         [string]$ldir
     )
 
-    return $ldir.Replace($rrB, $rrL)
+    return $ldir.Replace($rrB, $rrR)
 }
 
 
 
 
 ###########################################################
-# Main Caller
+# Function Caller
 ###########################################################
-main
+if ($action.ToLower() -eq "sort"){
+    sort_dir @PSBoundParameters
+} elseif ($action.ToLower() -eq "restore"){
+    restore_dir @PSBoundParameters
+} elseif ($action.ToLower() -eq "pack"){
+    pack2StaticRecall @PSBoundParameters
+} elseif ($action.ToLower() -eq "show"){
+    show_robs @PSBoundParameters
+} else {
+    show_help
+}
