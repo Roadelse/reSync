8550f176ee89c09257f715841040dad5d867e162
Roadelse <roadelse@HaSee-GX8.com>
2024-01-07 12:02:49 +0800

    v24.1.7, rebuild jsync and refine stem-comments in robsys.ps1

--- README.md
+++ README.md
@@ -1,50 +1,13 @@
-# rdSync
-synchronize files among Linux hosts, with binding directories specified along the path
+# reSync readme
 
-规划 : 手动传输文件 → 手动同步文件 → 自动同步文件
+**Roadelse synchronization universe**
 
-目前仍位于第一步
++ provide several tools for improving sync-relative affairs during daily work
 
-***
-***
-# readme
+1. **jsync**: project-level sync tool, a wrapper for rsync but using pre-configured hosts & paths to avoid typing explicit paths
 
-## v0.1.2
-
-+ 本地多路径的快速切换(主要是考虑多个同步盘协同工作的场景, 文件全分散), 后续支持同步功能(但是同步盘又不能用symlink...)
-+ 和若干远程目标地址绑定的快速数据传输功能, 暂时不支持sync, 尽管名字叫sync
-
-```
-Usage:
-  rdsync [options] files
-  options:
-    -m mode   , can be get, put and put2 (auto create missing directories), default put
-    -c config , specify config filename, default ".rdsync_config"
-    -t target , specify target machine:address, default any (i.e., the 1st)
-
-  rob [options] target
-    target : r(recRoot)/o(OneDrive)/b(BaiduSync)
-    options : 
-      -c, create the target directory if not existed
-```
-
-put的话可以多文件, get的话请用通配符, 同时别让他在本地就展开了! (没文件或者加单引号最保险)
-
-
+2. **robsys**: used to operate files & directories among local directory and corresponding cloud-sync dirs. Fixed for my custom r.o.b.s. distributed project FS
 
 ***
-***
 
-# Changelog
-
-## v0.1.2
-
-添加`rob`指令对本地多路径的快速切换(与自动化创建)
-
-## v0.1.1
-
-add 'showmode (-s)' to show the config file
-
-## v0.1
-
-init, 最基本的数据来回传输, 但是支持自动创建dir, 这个还是有点用
++ See detailed description and usage in each directory.
--- robsys/robs.ps1
+++ robsys/robs.ps1
@@ -1,14 +1,25 @@
 #Requires -Version 7
 
-# This script serves as a controller for files in recRoot/Roadelse
+# This script serves as a controller for files in r.o.b.s. system
 # including several main functionality:
 #    ● Split file/dir into Local/OneDrive/BaiduSYnc
 #    ● Union file/dir from OneDrive/BaiduSync
+#    ● Pack outdated directories into StaticRecall 
 
 # 2023-12-25    init
+# 2024-01-07    refined
 
 
-# >>>>>>>>>>>>>> params
+
+###########################################################
+# Preparation
+###########################################################
+
+# >>>>>>>>>>>>>>>>>>>>> pre-settings <<<<<<<<<<<<<<<<<<<<<<
+Import-Module rdee
+
+
+# >>>>>>>>>>>>>>>>>>>> params resolve <<<<<<<<<<<<<<<<<<<<<
 param (
     [Alias("e")]
     [switch]$echo_only,
@@ -24,32 +35,43 @@ param (
     [switch]$staticRecall
 )
 
-Import-Module rdee
 
-# >>>>>>>>>>>>>> global variables and pre-check
+# >>>>>>>>>>>>>>>>>>> global variables <<<<<<<<<<<<<<<<<<<<
+# ================== r.o.b.s. root paths
 $rrL = "D:\recRoot\Roadelse"
 $rrO = "C:\Users\${env:UserName}\OneDrive\recRoot\Roadelse"
 $rrB = "D:\BaiduSyncdisk\recRoot\Roadelse"
 $rrS = "D:\recRoot\StaticRecall"
 
+# ================== key variables in processing
+# $errlog = "${target_dir}\resort-error." + (Get-Date -Format "yyyyMMdd-HHmmss") + ".log"
+$ignore_global = @(".reconf$", ".resync_config$", "resort-error\..*\.log$", "^prior\.", "^deprecated\.", ".obsidian")
+[System.Collections.ArrayList]$sorted_dirs = @()
+
+
+# >>>>>>>>>>>>>>>>>>>> pre-processing <<<<<<<<<<<<<<<<<<<<<
+# ================== set $pwd as default target if not specified
 if ($target -eq "") {
     $target = "."
 }
 
+# ================== check target existence
 if (-not (Test-Path -Path $target)) {
     Write-Output "target path doesn't exist!"
     exit 1
 }
 
+# ================== get target directory path
 $target_dir = (Get-Item $target).FullName
-if ((Get-Item $target_dir).PSIsContainer) {
-    # $target_dir = (Get-Item $target_dir).FullName
-} elseif ((Get-Item $target).Name -eq ".reconf") {
+if ((Get-Item $target_dir).PSIsContainer) { #>- pass for directory
+    # pass
+} elseif ((Get-Item $target).Name -eq ".reconf") {  #>- get directory path for .reconf
     $target_dir = (Get-Item $target_dir).DirectoryName
-} else {
+} else { #>- error for file not being .reconf
     Write-Error "If a file is given, is must be a .reconf file!" -ErrorAction Stop
 }
 
+# ================== convert all obs path to roadelse path, i.e., local path
 if ($target.FullName.StartsWith($rrO)) {
     $target_dir = $target_dir.Replace($rrO, $rrL)
 } elseif ($target.FullName.StartsWith($rrB)) {
@@ -57,15 +79,17 @@ if ($target.FullName.StartsWith($rrO)) {
 } elseif ($target.FullName.StartsWith($rrS)) {
     $target_dir = $target_dir.Replace($rrS, $rrL)
 } elseif ($target.FullName.StartsWith($rrL)) {
-} else {
+} else {  #>- Error if not within r.o.b.s. system
     Write-Error "Path not in r.o.b.s. system! $target_dir" -ErrorAction Stop
 }
 
-$errlog = "${target_dir}\resort-error." + (Get-Date -Format "yyyyMMdd-HHmmss") + ".log"
-$ignore_global = @(".reconf$", ".resync_config$", "resort-error\..*\.log$", "^prior\.", "^deprecated\.", ".obsidian")
-[System.Collections.ArrayList]$sorted_dirs = @()
 
-# >>>>>>>>>>>>>> core functions
+
+
+###########################################################
+# kernel functions
+###########################################################
+# >>>>>>>>>>>>>>>>>>>>>>>>> entry <<<<<<<<<<<<<<<<<<<<<<<<<
 function main {
     
     if ($restore_mode) {
@@ -78,10 +102,16 @@ function main {
 }
 
 
+# >>>>>>>>>>> sort content in target directory <<<<<<<<<<<<
 function sort_dir {
-    # params:
-    #    wdir - working directory
+    <# .SYNOPSIS
+    This function aims to sort all files/directories in the target directory. That is, move files/dirs into OneDrive and BaiduSync paths and link them back based on several rules. 
+    
+    .PARAMETER wdir
+    target working directory
+    #>
 
+    # ================== parameters definition
     param(
         [Parameter(Mandatory = $true)]
         $wdir,
@@ -90,7 +120,8 @@ function sort_dir {
 
     # Write-Output "Enter sort_dir $wdir"
 
-    # keep $wdir be an item object 
+    # ================== pre-processing
+    # ~~~~~~~~~~ handle r.o.b. paths 
     if ($wdir -is [string]) {
         if (-not(Test-Path $wdir)) { #>- for conditions that building local links in a new PC
             New-Item -ItemType Directory $wdir -Force
@@ -101,38 +132,41 @@ function sort_dir {
     $odir_path = l2o $wdir.FullName
     $bdir_path = l2b $wdir.FullName
 
-    # load .reconf if existed
+    # ~~~~~~~~~~ load .reconf if existed
     if (Test-Path "${wdir}\.reconf") {
         Update-Hashtable $rcf (Get-Content "${wdir}\.reconf" | ConvertFrom-Json -AsHashtable)
     }
     
-    # >>> check ignore_this
+    # ~~~~~~~~~~ check ignore_this and 
     if ($rcf.ignore_this) {
         return
     }
+
+    # ~~~~~~~~~~ remark current target in global
     $sorted_dirs.Add($wdir.FullName.Replace($rrL + "\", "")) | Out-Null
 
-    # >>> check ignore_list & get target list
+    # ================== handle children items one by one
     Get-ChildItem $wdir | 
     ForEach-Object -Process {
-        # 1. ignore symlink
+        # ~~~~~~~~~~ ignore symlink
         if ($_.Attributes -match "ReparsePoint") {
             return
         }
 
         # Write-Output "processing $_"
-        # 2. check if ignore_this
+        # ~~~~~~~~~~ check ignore_global
         foreach ($ig in $ignore_global) {
             if ($_.Name -match $ig) {
                 return
             }
         }
 
+        # ~~~~~~~~~~ check ignore list in rcf
         if ($rcf.Contains('ignore_list') -and $rcf.ignore_list.Contains($_.Name)) { #>- manual ignore
             return
         }
 
-        # 3. check manually set Onedrive / Baidusync (no-condition)
+        # ~~~~~~~~~~ Do the operation via manually set rule
         if ($rcf.Contains('OneDrive') -and $rcf.OneDrive.Contains($_.Name)) {
             move_and_link $_ $_.FullName.Replace($rrL, $rrO)
             return
@@ -141,7 +175,7 @@ function sort_dir {
             return
         }
 
-        # 4. check FDs with specific prefix O.. or B..
+        # ~~~~~~~~~~ Do the operation via name prefix rule
         if ($_.Name.StartsWith("O..")) {
             move_and_link $_ $_.FullName.Replace($rrL, $rrO)
             return
@@ -150,9 +184,11 @@ function sort_dir {
             return
         }
 
-        # 5. split File/Directory
+        # ~~~~~~~~~~ handle children dir recursively
         if ($_.PSIsContainer) {
             sort_dir -wdir $_ -rcf (deepcopy $rcf)
+
+        # ~~~~~~~~~~ Do the operation in default rules
         } else {
             $fsize = $_.Length / 1MB; # file size in MB
             if ($fsize -lt 5) {
@@ -164,16 +200,18 @@ function sort_dir {
         # exit 0
     }
 
+    # ================== link back items in OneDrive
     if (Test-Path $odir_path) {
         Get-ChildItem $odir_path | ForEach-Object -Process {
             $_lp = o2l $_.FullName  #>- corresponding local path for $_.FullName
             if (Test-Path $_lp) {
-                # --- handle shortcut links separately (should only in onedrive!)
+                # ~~~~~~~~~~ handle shortcut links separately (should only in onedrive!)
                 if ($_lp.EndsWith(".lnk") -and (-not (Test-Path $_lp))) {
                     Copy-Item -Path $_.FullName -Destination $_lp
                     continue
                 }
 
+                # ~~~~~~~~~~ handle error conditions
                 $i_lp = Get-Item $_lp
                 if (-not ($i_lp.Attributes -match "ReparsePoint")) {
                     if ($i_lp.PSIsContainer) {
@@ -185,6 +223,7 @@ function sort_dir {
                     }
                 }
             } else {
+                # ~~~~~~~~~~ do the link operation
                 if ($verbose) {
                     Write-Output ("link OneDrive item: " + $_lp.Replace($rrL + "\", "") + " to Local")
                 }
@@ -193,11 +232,13 @@ function sort_dir {
         }
     }
 
+    # ================== link back items in BaiduSync
     if (Test-Path $bdir_path) {
         Get-ChildItem $bdir_path | ForEach-Object -Process {
             Assert (-not $_.FullName.EndsWith(".lnk")) "Shortcut links should not occur in BaiduSync!" #>- ensure no shortcut link
             $_lp = b2l $_.FullName  #>- corresponding local path for $_.FullName
             if (Test-Path $_lp) {
+                # ~~~~~~~~~~ handle error conditions
                 $i_lp = Get-Item $_lp
                 if (-not ($i_lp.Attributes -match "ReparsePoint")) {
                     if ($i_lp.PSIsContainer) {
@@ -209,6 +250,7 @@ function sort_dir {
                     }
                 }
             } else {
+                # ~~~~~~~~~~ do the link operation
                 if ($verbose) {
                     Write-Output ("link BaiduSync item: " + $_lp.Replace($rrL + "\", "") + " to Local")
                 }
@@ -218,12 +260,44 @@ function sort_dir {
     }
 }
 
+
+# >>>>>>>>>>>>>>>>> execution in sort_dir <<<<<<<<<<<<<<<<<
+function move_and_link([object]$src, [string]$dst) {
+    Write-Output "move_and_link($src, $dst)"
+
+    # ================== ensure $dst doesn't exist
+    if (Test-Path $dst) {
+        errHandler "Destination already exists! $dst"
+    }
+    if ($echo_only) {
+        return
+    }
+
+    New-Item -ItemType Directory (Split-Path $dst) -Force > $null
+
+    # ================== for shortcut, just copy it
+    if ($src.Name.EndsWith(".lnk")) {
+        Copy-Item -Path $src.FullName -Destination "$dst"
+        return
+    }
+
+    # ================== move then link back
+    Move-Item -Path $src.FullName -Destination $dst
+    New-Item -ItemType SymbolicLink -Path $src.FullName -Target $dst > $null
+    
+    # exit 0
+}
+
+
+# >>>>>>>>>>>>>>>>>>>> pack dir to SR <<<<<<<<<<<<<<<<<<<<<
 function pack2StaticRecall {
+    # ================== param resolve
     param(
         [Parameter(Mandatory = $true)]
         [string]$ldir
     )
 
+    # ================== pre-processing
     $odir = l2o $ldir
     $bdir = l2b $ldir
 
@@ -238,6 +312,8 @@ function pack2StaticRecall {
         Write-Output "compress & delete, move .zip to StaticRecall"
         return
     }
+
+    # ================== do the operation
     Compress-Archive -Path $ldir -DestinationPath ($ldir + ".zip") -CompressionLevel Fastest
     Remove-Item $ldir -Recurse -Force
     New-Item -ItemType Directory -Path (Split-Path $sr_path) -Force
@@ -245,41 +321,9 @@ function pack2StaticRecall {
 }
 
 
-function errHandler {
-    Param(
-        [Parameter(Mandatory = $true)]
-        [string]$errmsg
-    )
-    Write-Error "Error: $errmsg" ($error_stop ? (-ErrorAction Stop) : (-ErrorAction continue))
-}
-
-function move_and_link([object]$src, [string]$dst) {
-    Write-Output "move_and_link($src, $dst)"
-
-    # >>> ensure $dst doesn't exist
-    if (Test-Path $dst) {
-        errHandler "Destination already exists! $dst"
-    }
-    if ($echo_only) {
-        return
-    }
-
-    New-Item -ItemType Directory (Split-Path $dst) -Force > $null
-
-    # >>> for shortcut, just copy it
-    if ($src.Name.EndsWith(".lnk")) {
-        Copy-Item -Path $src.FullName -Destination "$dst"
-        return
-    }
-
-    # >>> move then link back
-    Move-Item -Path $src.FullName -Destination $dst
-    New-Item -ItemType SymbolicLink -Path $src.FullName -Target $dst > $null
-    
-    # exit 0
-}
-
+# >>>>>>>>>>>>>>> gather files back to <r.> <<<<<<<<<<<<<<<
 function restore_dir {
+    # ================== param resolve
     [CmdletBinding()]
     param (
         [string]$wdir,
@@ -287,6 +331,7 @@ function restore_dir {
         [string]$mc = "Copy"
     )
 
+    # ================== pre-processing
     if ($echo_only) {
         Write-Output "restore_dir($wdir, $mc)"
         return
@@ -294,6 +339,7 @@ function restore_dir {
 
     $wdir = $wdir -is [string] ? (Get-Item $wdir) : $wdir
 
+    # ================== handle <r.> items one by one
     Get-ChildItem $wdir | 
     ForEach-Object -Process {
         if ($_.Attributes -match "ReparsePoint") {
@@ -316,7 +362,7 @@ function restore_dir {
         }
     }
 
-    # if ($mc.ToUpper() -eq "MOVE") {  # To-be-considered, depend on how I handle the remained files @2023-12-26 11:31:04
+    # ================== handle <o.> items one by one
     if (Test-Path (l2o $wdir)) {
         $nFilesO = (Get-ChildItem -Path (l2o $wdir) -Recurse -File | Measure-Object).Count
         if ($nFilesO -gt 0) {
@@ -325,6 +371,8 @@ function restore_dir {
             Remove-Item (l2o $wdir) -Recurse -Force
         }
     }
+
+    # ================== handle <b.> items one by one
     if (test-path (l2b $wdir)) {
         $nFilesB = (Get-ChildItem -Path (l2b $wdir) -Recurse -File | Measure-Object).Count
         if ($nFilesB -gt 0) {
@@ -333,10 +381,20 @@ function restore_dir {
             Remove-Item (l2b $wdir) -Recurse -Force
         }
     }
-    # }
-
 }
 
+
+# >>>>>>>>>>>>>>>> error handler function <<<<<<<<<<<<<<<<<
+function errHandler {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$errmsg
+    )
+    Write-Error "Error: $errmsg" ($error_stop ? (-ErrorAction Stop) : (-ErrorAction continue))
+}
+
+
+# >>>>>>>>>>>>>>>> path-transfer functions <<<<<<<<<<<<<<<<
 function l2o {
     param(
         [Parameter(Mandatory = $true)]
@@ -349,7 +407,7 @@ function o2l {
     param(
         [Parameter(Mandatory = $true)]
         [string]$ldir
-    )
+    ) 
 
     return $ldir.Replace($rrO, $rrL)
 }
@@ -374,4 +432,8 @@ function b2l {
 
 
 
+
+###########################################################
+# Main Caller
+###########################################################
 main
